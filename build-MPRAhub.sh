1241df4bec815ed4043cb9745ada9dd6cabf1611
Andrew Blair <andrew.blair@ucsf.edu>
2023-05-20 22:52:02 -0700

    Add MPRAhub build

--- build-MPRAhub.sh
+++ build-MPRAhub.sh
@@ -3,6 +3,8 @@ mkdir MPRAhub
 cd MPRAhub
 
 git clone git@github.com:MPRAhub/MPRAbase-Builder.git
-git clone git@github.com:MPRAhub/MPRAbase.git
 git clone git@github.com:MPRAhub/iSEEindex.git
+git clone git@github.com:MPRAhub/MPRAbase.git
 
+cd ../env/
+bash build-MPRAbase-image.sh
