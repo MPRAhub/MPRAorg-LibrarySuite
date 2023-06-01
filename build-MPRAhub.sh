486c7587dadd70aee92954228ac1c9c74ca45c89
apblair <apblair95@gmail.com>
2023-06-01 15:39:08 -0700

    Update build-MPRAhub.sh

--- build-MPRAhub.sh
+++ build-MPRAhub.sh
@@ -6,5 +6,3 @@ git clone git@github.com:MPRAhub/MPRAbase-Builder.git
 git clone git@github.com:MPRAhub/iSEEindex.git
 git clone git@github.com:MPRAhub/MPRAbase.git
 
-cd ../env/
-bash build-MPRAhub-image.sh
