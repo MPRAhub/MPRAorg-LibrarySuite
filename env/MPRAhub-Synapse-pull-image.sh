6519dd7abed12fe76a4a58dfd712a37b18230363
apblair <apblair95@gmail.com>
2023-06-02 13:10:16 -0700

    Update env build scripts

---
+++ env/MPRAhub-Synapse-pull-image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# NOTE: The Synapse MPRAbase Docker registry requires you
+# to login before push the container.
+# docker login -u <username> -p <password> docker.synapse.org
+# docker pull docker.synapse.org/syn51118207/mprabase:amd64-v0.1.0
+docker pull docker.synapse.org/syn51118207/mprabase:arm64-v0.1.1
