9c3fa139f4cc085b57bc96df5f627e4655a9a3f1
apblair <apblair95@gmail.com>
2023-06-02 13:12:54 -0700

    rm old naming

--- env/build-MPRAhub-image.sh
+++
@@ -1,1 +0,0 @@
-docker build -t docker.synapse.org/syn51118207/mprabase:arm64-v0.1.1 . # NOTE: For m1/m2 iOS
--- env/build-MPRAhub-sif.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# NOTE: If not already done, please connect your Docker account to your Synapse account
-# singularity pull --docker-login docker://docker.synapse.org/syn51118207/mprabase:amd64-v0.1.0
-singularity build --docker-login MPRAhub.sif MPRAhub.def 
-#docker://docker.synapse.org/syn51118207/mprabase:amd64-v0.1.0
